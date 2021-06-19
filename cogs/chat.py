168ed8be875eb9574fd0ba15f95c5967762242f9
KaiNiGHt <68188784+KaiNiGHt@users.noreply.github.com>
2021-06-19 23:01:21 +0900

    번역 예토전생

--- cogs/chat.py
+++ cogs/chat.py
@@ -19,36 +19,36 @@ class Chat(commands.Cog):
 
     # BUG self.translator.detect(text)에 AttributeError: 'NoneType' object has no attribute 'group' 오류
 
-    # @commands.command(name='번역', aliases=['trans', 'translate'])
-    # async def translate_command(self, ctx, *, text):
-    #     await ctx.trigger_typing()
+    @commands.command(name='번역', aliases=['trans', 'translate'])
+    async def translate_command(self, ctx, *, text):
+        await ctx.trigger_typing()
 
-    #     language = self.translator.detect(text).lang
-    #     translate = self.translator.translate
+        language = self.translator.detect(text).lang
+        translate = self.translator.translate
 
-    #     if language == 'ko': 
-    #         translate_result = translate(text)
+        if language == 'ko': 
+            translate_result = translate(text)
 
-    #         result_text = translate_result.text
-    #         result_pronunciation = translate_result.pronunciation
+            result_text = translate_result.text
+            result_pronunciation = translate_result.pronunciation
 
-    #     else:
-    #         translate_result = translate(text, dest = 'ko')
+        else:
+            translate_result = translate(text, dest = 'ko')
 
-    #         result_text = translate_result.text
-    #         result_pronunciation = translate_result.pronunciation
+            result_text = translate_result.text
+            result_pronunciation = translate_result.pronunciation
 
-    #     embed = discord.Embed(
-    #         title='번역 결과',
-    #         description=f'''번역 결과는 정확하지 않을 수 있습니다.
+        embed = discord.Embed(
+            title='번역 결과',
+            description=f'''번역 결과는 정확하지 않을 수 있습니다.
 
-    #         [구글 번역기](https://translate.google.com/)''',
-    #         color=discord.Colour.blurple()
-    #     )
+            [구글 번역기](https://translate.google.com/)''',
+            color=discord.Colour.blurple()
+        )
         
-    #     embed.add_field(name=result_text, value=f'"{result_pronunciation}"')
+        embed.add_field(name=result_text, value=f'"{result_pronunciation}"')
 
-    #     await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Chat(bot))
