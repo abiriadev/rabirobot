dab597365c9ff659577c0541663db6ec125ff419
KaiNiGHt <rt.kirby2009@gmail.com>
2021-06-19 21:42:18 +0900

    first

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord import message
+from discord.ext import commands
+from discord import User
+import asyncio
+import os
+bot = commands.Bot(command_prefix='r/')
+#--------------------------------------------
+os.system("cls")
+@bot.event
+async def on_ready():
+    print(bot.user.name, '봇이 준비되었습니다!')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('봇 테스트'))
+    print('[=========]')
+    print('LOG / ERROR')
+
+@bot.command()
+async def ping(ctx):
+    # await ctx.send(f'퐁! / 현재 핑 : {round(bot.latency * 1000)}ms')
+    await ctx.send(embed=discord.Embed(
+        title=" :ping_pong: pong!",
+        description=f" ping : {round(bot.latency * 1000)}ms",
+        color=discord.Colour.default()))
+
+#------------------------------------------------------------------------
+bot.run('ODU1NjUyODM3MjM2NjcwNDY0.YM1mzQ.2fiCLc8Tlc23ps-IrQ4NVQArCnc')
